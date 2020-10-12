33fbdcef5ccc390552a82b52f4f32c5bfdbe42f5
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-2.local>
2020-10-12 22:30:36 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index" :class="task.Priority">
+        <span>{{ task.name }}</span>
+
+
+      </li>
+
+
+    </ul>
+
+    <form v-on:submit.prevent="saveNewTask">
+      <label for="new-task">Add a new task</label>
+      <input type="text" id="new-task" v-model="newTask.name" />
+      <label for="priority">Priority:
+      <label for="high-priority">High</label>
+      <input type="radio" id="high-priority" name="priority" value="High" v-model="newTask.Priority" />
+      <label for="low-priority">Low</label>
+      <input type="radio" id="low-priority" name="priority" value="Low" v-model="newTask.Priority" /></label>
+      <input type="submit" value="Save New Task">
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data(){
+    return {
+      tasks: [
+        {name:"Buy shopping", Priority: "Low"},
+        {name:"Clean bathroom", Priority: "Low"},
+        {name:"Car's MOT", Priority: "High"},
+      ],
+      newTask: {
+        name: "",
+        Priority: null
+      },
+    }
+  },
+  methods: {
+    saveNewTask: function () {
+      this.tasks.push(this.newTask);
+      this.newTask = {
+        name: "",
+        Priority: null
+      }
+    },
+  },
+
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 60%;
+  margin: 0 auto;
+  font-family: 'Lato', sans-serif;
 }
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+li {
+  margin: 20px 0;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+li span {
+  padding: 8px;
+}
+
+li button {
+  background: #f2360c;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+}
+
+li.Low {
+  border: 2px solid #1a681e;
+}
+
+li.High {
+  border: 2px solid #f2360c;
+}
+
+input[type="text"] {
+  padding: 10px;
+  width: 50%;
+  margin:5px;
+}
+
+button, input[type="submit"]{
+  padding: 10px;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #000;
+}
+
 </style>
